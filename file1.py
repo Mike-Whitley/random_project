794613d24fadaa743ec46b5a4e7cf48c345aae14
Mike Whitley <michaelwhitley11@gmail.com>
2024-05-21 19:32:41 +1200

    file1 update

--- file1.py
+++ file1.py
@@ -1,5 +1,5 @@
-from gevent import monkey
-monkey.patch_all()
+#from gevent import monkey
+#monkey.patch_all()
 
 print("Gevent is installed and working correctly.")
 
@@ -452,9 +452,9 @@ class App(object):
         stop.grid(row=0, column=1, padx=(40, 40), pady=(40, 40))
 
         start.grid()
-        stop.grid()
+        #stop.grid()
 
-        self.root.after(1, self.trading)  # After 1 second, call scanning
+        #self.root.after(1, self.trading)  # After 1 second, call scanning
         self.root.mainloop()
 
 
@@ -464,46 +464,50 @@ app.run()
 
 
 
-monkey.patch_all()
+# monkey.patch_all()
 
 
-import nashpy as nash
-import numpy as np
+# import nashpy as nash
+# import numpy as np
 
 
-A = np.array([[165 / 100, -1],
-              [-1, 160 / 100]])
+# A = np.array([[165 / 100, -1],
+#               [-1, 160 / 100]])
 
-'''A = np.array([[185/100,-1, -1],
-              [-1,  125/100, -1],
-              [-1,-1, 280/100]])'''
+# '''A = np.array([[185/100,-1, -1],
+#               [-1,  125/100, -1],
+#               [-1,-1, 280/100]])'''
 
-'''A = np.array([[100/110,-1,-1,-1,-1],
-              [-1,300/100,-1,-1,-1],
-              [-1,-1,1300/100,-1,-1],
-              [-1,-1,-1,200/100,-1],
-              [-1,-1,-1,-1,1300/100]])'''
+# '''A = np.array([[100/110,-1,-1,-1,-1],
+#               [-1,300/100,-1,-1,-1],
+#               [-1,-1,1300/100,-1,-1],
+#               [-1,-1,-1,200/100,-1],
+#               [-1,-1,-1,-1,1300/100]])'''
 
-rps = nash.Game(A)
+# rps = nash.Game(A)
 
-eqs = rps.support_enumeration()
-result = list(eqs)[0][0]
-print(result)
+# eqs = rps.support_enumeration()
+# result = list(eqs)[0][0]
+# print(result)
 
-bet_amount = 100
+# bet_amount = 100
 
-result = bet_amount * result
-result = [round(x) for x in result]
+# result = bet_amount * result
+# result = [round(x) for x in result]
 
-bet_amount = sum(result)
+# bet_amount = sum(result)
 
-print(result)
+# print(result)
 
-sum_val = 0
+# sum_val = 0
 
-for m in range(len(result)):
-    print((result[m] * A[m][m] / 100 + result[m]) <= bet_amount)
-    sum_val += sum(result[m] * A[m])
+# for m in range(len(result)):
+#     print((result[m] * A[m][m] / 100 + result[m]) <= bet_amount)
+#     sum_val += sum(result[m] * A[m])
 
-return_val = sum_val/len(result)
-print(return_val, return_val/bet_amount)
+# return_val = sum_val/len(result)
+# print(return_val, return_val/bet_amount)
+
+
+# if __name__ == '__main__':
+#     pass
